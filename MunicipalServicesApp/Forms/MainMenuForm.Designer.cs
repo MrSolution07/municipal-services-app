281dd71c2a85ee6b217b6615d9c93e3266fff959
Christian Bulabula Emungu <97136008+MrSolution07@users.noreply.github.com>
2026-09-02 08:57:14 +0200

    feat: layout main menu header and body

--- MunicipalServicesApp/Forms/MainMenuForm.Designer.cs
+++ MunicipalServicesApp/Forms/MainMenuForm.Designer.cs
@@ -24,3 +24,83 @@ namespace MunicipalServicesApp.Forms
 
             base.Dispose(disposing);
         }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubtitle = new System.Windows.Forms.Label();
+            this.layoutBody = new System.Windows.Forms.TableLayoutPanel();
+            this.lblChoose = new System.Windows.Forms.Label();
+            this.btnReportIssues = new System.Windows.Forms.Button();
+            this.btnLocalEvents = new System.Windows.Forms.Button();
+            this.btnServiceStatus = new System.Windows.Forms.Button();
+            this.lblSessionCount = new System.Windows.Forms.Label();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.panelHeader.SuspendLayout();
+            this.layoutBody.SuspendLayout();
+            this.SuspendLayout();
+            //
+            // panelHeader
+            //
+            this.panelHeader.Controls.Add(this.lblSubtitle);
+            this.panelHeader.Controls.Add(this.lblTitle);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Padding = new System.Windows.Forms.Padding(18, 14, 18, 14);
+            this.panelHeader.Size = new System.Drawing.Size(704, 86);
+            this.panelHeader.TabIndex = 0;
+            //
+            // lblTitle
+            //
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(18, 14);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(360, 30);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Municipal Services - South Africa";
+            //
+            // lblSubtitle
+            //
+            this.lblSubtitle.AutoSize = true;
+            this.lblSubtitle.Location = new System.Drawing.Point(21, 50);
+            this.lblSubtitle.Name = "lblSubtitle";
+            this.lblSubtitle.Size = new System.Drawing.Size(420, 17);
+            this.lblSubtitle.TabIndex = 1;
+            this.lblSubtitle.Text = "Main menu - choose the service you need help with.";
+            //
+            // layoutBody
+            //
+            this.layoutBody.ColumnCount = 1;
+            this.layoutBody.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.layoutBody.Controls.Add(this.lblChoose, 0, 0);
+            this.layoutBody.Controls.Add(this.btnReportIssues, 0, 1);
+            this.layoutBody.Controls.Add(this.btnLocalEvents, 0, 2);
+            this.layoutBody.Controls.Add(this.btnServiceStatus, 0, 3);
+            this.layoutBody.Controls.Add(this.lblSessionCount, 0, 4);
+            this.layoutBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutBody.Location = new System.Drawing.Point(0, 86);
+            this.layoutBody.Name = "layoutBody";
+            this.layoutBody.Padding = new System.Windows.Forms.Padding(24, 18, 24, 18);
+            this.layoutBody.RowCount = 5;
+            this.layoutBody.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.layoutBody.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.layoutBody.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.layoutBody.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34F));
+            this.layoutBody.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.layoutBody.Size = new System.Drawing.Size(704, 375);
+            this.layoutBody.TabIndex = 1;
+            //
+            // lblChoose
+            //
+            this.lblChoose.AutoSize = true;
+            this.lblChoose.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblChoose.Location = new System.Drawing.Point(27, 18);
+            this.lblChoose.Name = "lblChoose";
+            this.lblChoose.Size = new System.Drawing.Size(650, 34);
+            this.lblChoose.TabIndex = 0;
+            this.lblChoose.Text = "Select one of the three municipal services below.";
+            this.lblChoose.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            //
