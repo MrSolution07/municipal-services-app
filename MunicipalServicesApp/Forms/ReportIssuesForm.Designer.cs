8832dc848a7cd36828693a81cd2e36370be4ae91
Christian Bulabula Emungu <97136008+MrSolution07@users.noreply.github.com>
2026-09-02 08:57:14 +0200

    feat: declare report form controls

---
+++ MunicipalServicesApp/Forms/ReportIssuesForm.Designer.cs
@@ -0,0 +1,39 @@
+namespace MunicipalServicesApp.Forms
+{
+    partial class ReportIssuesForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubtitle;
+        private System.Windows.Forms.TableLayoutPanel layoutBody;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cboCategory;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.RichTextBox rtbDescription;
+        private System.Windows.Forms.Label lblAttachment;
+        private System.Windows.Forms.Panel panelAttachment;
+        private System.Windows.Forms.Label lblAttachmentName;
+        private System.Windows.Forms.Button btnAttachFile;
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.TableLayoutPanel layoutEngagement;
+        private System.Windows.Forms.ProgressBar progressReport;
+        private System.Windows.Forms.Label lblEngagement;
+        private System.Windows.Forms.Panel panelButtons;
+        private System.Windows.Forms.Button btnBackToMenu;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ToolTip toolTip;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
