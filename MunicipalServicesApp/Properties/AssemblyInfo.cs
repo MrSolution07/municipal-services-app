2166ec5ccacf867e04fd0bf2c400f8d04a08b565
Christian Bulabula Emungu <97136008+MrSolution07@users.noreply.github.com>
2026-09-02 08:57:14 +0200

    chore: add assembly metadata

---
+++ MunicipalServicesApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MunicipalServicesApp")]
+[assembly: AssemblyDescription("Municipal Services Application for South Africa - Part 1 (Report Issues)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MunicipalServicesApp")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6b1d8f3a-2c4e-4a71-9f0c-53d8b7c2a410")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
