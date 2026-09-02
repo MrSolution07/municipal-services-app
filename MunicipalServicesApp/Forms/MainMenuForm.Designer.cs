1bd00dd255aecf0f2f698a906e4520be2efd2c30
Christian Bulabula Emungu <97136008+MrSolution07@users.noreply.github.com>
2026-09-02 08:57:14 +0200

    feat: declare main menu controls

---
+++ MunicipalServicesApp/Forms/MainMenuForm.Designer.cs
@@ -0,0 +1,26 @@
+namespace MunicipalServicesApp.Forms
+{
+    partial class MainMenuForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubtitle;
+        private System.Windows.Forms.TableLayoutPanel layoutBody;
+        private System.Windows.Forms.Label lblChoose;
+        private System.Windows.Forms.Button btnReportIssues;
+        private System.Windows.Forms.Button btnLocalEvents;
+        private System.Windows.Forms.Button btnServiceStatus;
+        private System.Windows.Forms.Label lblSessionCount;
+        private System.Windows.Forms.ToolTip toolTip;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
